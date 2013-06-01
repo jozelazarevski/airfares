a9e1c73943c10c7894f0c5aa9658a664f430d32b
Boris Demeshev <boris.demeshev@gmail.com>
2013-06-01 11:27:56 +0400

    catch the bug!

--- R/07_catch_the_bug.R
+++ R/07_catch_the_bug.R
@@ -1,58 +1,78 @@
 # Parsing sindbad.ru
 
-# probably there is an error in 01_create_big_table.R
-# so we search for suspicious observations
+# first _actual_ computer bug was found by a woman, Grace Hopper:
+# http://ids.si.edu/ids/dynamic?id=NMAH-92-13130&container.fullpage
 
+# catch the bug in file num 100 (!)
+
+
+#### RESULT:
+
+#  It's not a BUG, it's an undocumented FEATURE! 
+#  RAW data may contain the SAME flight TWICE! 
+
+#  I don't know why. One needs to find such an example live on sindbad.
+#  So the replacement comp_name>0 ====> comp_name=1 seems OK
+
+#####
+
+
+
+# Convert all the files (check!)
+# flights_xxx.csv, companies_xxx.csv, price_xxx.csv
+# into one big "file.out" (without dummy variables!)
 
 file.out <- "all_fly_data.csv"
 
 # the flights file should have 'parse_time' column
 
-setwd('~/courseworks/2013/annette/airfares/rawdata/')
+setwd('~/courseworks/2013/annette/airfares/rawdata/problem_01/')
 
 library(lubridate)
 
 
 begin.time <- now()
 
-################################################
-### get all the filenames to convert...
-
-# get all the file names that starts with "flights_"
-fly.files.list <- list.files(pattern="^flights_")
-
-# check
-fly.files.list
-
-# in the future I should learn the power of regular expressions :)
-# https://xkcd.com/208/
-
-n.files <- length(fly.files.list) # number of files to convert
-file.nums <- rep(0,n.files) # internal files numbers
-# they may be not consecutive as we may parse from different computers
-
-# without regular expressions I am doing somesing nasty:
-for (i in 1:n.files) {
-  part <- strsplit(fly.files.list[i],"_",fixed=TRUE)[[1]][2] # take the part after "_"
-  number <- as.numeric(strsplit(part,".",fixed=TRUE)[[1]][1]) # take the part before "."
-  file.nums[i] <- number
-  # here python beats R :)
-}
-
-
-# now we should have all the file numbers
-# file.nums
+# ################################################
+# ### get all the filenames to convert...
+# 
+# # get all the file names that starts with "flights_"
+# fly.files.list <- list.files(pattern="^flights_")
+# 
+# # check
+# fly.files.list
+# 
+# # in the future I should learn the power of regular expressions :)
+# # https://xkcd.com/208/
+# 
+# n.files <- length(fly.files.list) # number of files to convert
+# file.nums <- rep(0,n.files) # internal files numbers
+# # they may be not consecutive as we may parse from different computers
+# 
+# # without regular expressions I am doing somesing nasty:
+# for (i in 1:n.files) {
+#   part <- strsplit(fly.files.list[i],"_",fixed=TRUE)[[1]][2] # take the part after "_"
+#   number <- as.numeric(strsplit(part,".",fixed=TRUE)[[1]][1]) # take the part before "."
+#   file.nums[i] <- number
+#   # here python beats R :)
+# }
+# 
+# 
+# # now we should have all the file numbers
+# # file.nums
 
 ############################################
 ## convert 3 files into one big table WITHOUT dummies
 
 # let's work with one file
-# file.num <- 1000002
 
 
-happy.end <- NULL # No happy end exists... yet ;)
+file.num <- 1000100
 
-for (file.num in file.nums) {
+
+# happy.end <- NULL # No happy end exists... yet ;)
+
+# for (file.num in file.nums) {
 
   # create file names
   price.file.name <- paste("price_",as.character(file.num),".csv",sep="")
@@ -110,6 +130,17 @@ for (file.num in file.nums) {
   # for each company in company.df we find its index in price.df
   company.df$price.index <- rep(price.df$X,times=price.df$ncomp)
   
+
+  ###### SUBSET the data to find the BUG!
+
+  flight.df <- subset(flight.df,price.index==48)
+  company.df <- subset(company.df,price.index==48)
+  price.df <- subset(price.df,X==48)
+  # when I went to my school it's number was 48
+  # Maybe there lie all my problems? :)
+
+
+
   #####################
   # classify flights as "allez" and "retour" 
   
@@ -124,15 +155,31 @@ for (file.num in file.nums) {
   # classify flights as "allez" or "retour"
   flight.df$direction <- rep(ararar,times=opts12)
   
+
+  #  It's not a BUG, it's a FEATURE! 
+  #  RAW data may contain the SAME flight TWICE!
+ 
+  print(flight.df)
+  
+  # NO merges have been done yet!
+
+
   ######################
   # match "allez" and "retour"
   
   flight.allez.df <- subset(flight.df,direction=="allez")
   flight.retour.df <- subset(flight.df,direction=="retour")
-  
+
+
+
+  ##### than try ordinary way
+
   all.flight.df <- merge(flight.allez.df,flight.retour.df,
                          by.x="price.index",by.y="price.index")
-  
+
+
+  #### The BUG is earlier than THIS comment!
+
   # check that the merge is ok
   if (nrow(all.flight.df)!=sum(price.df$options1*price.df$options2))
     print("ACHTUNG! The number of created allez-retour options does not match price file")
@@ -150,22 +197,17 @@ for (file.num in file.nums) {
   # merging company names to price.df
   
   company.price.df <- merge(company.df,price.df,by.x="price.index",by.y="X")
-  
+
   ############
   # THE MERGE!
   current.table <- merge(all.flight.df,company.price.df,by.x="price.index",by.y="price.index")
   
   
-  bad.obs <- subset(current.table,
-                      (price==63450)&(dur_hours.x==6)&(dur_minutes.x==5) & (price.index==48) &
-                        (dur_hours.y==6)&(dur_minutes.y==25))
-  
-  if (nrow(bad.obs)>0) cat("Problems arise from file ",file.num,"\n")
-  
-#  # Happy End!!! Ils se marièrent et eurent beaucoup d'enfants!
+  # Happy End!!! Ils se marièrent et eurent beaucoup d'enfants!
 #  happy.end <- rbind(happy.end,current.table)
-}
+# }
 
+# write.csv(happy.end,file=file.out)
 
 end.time <- now()
 
